3f1f4004655de305626c5e5fee9c380df91c7c75
Elisabeth Irgens <hello@elisabethirgens.com>
2018-02-01 20:04:11 +0100

    Print actual user input and not just numbers 😆
    I had been testing in the terminal with quickly typing 1-9 so
    didn't see that the script was ignoring the input from the player.

--- tictactoe.py
+++ tictactoe.py
@@ -2,8 +2,8 @@ print("Welcome! Let’s play a game of tic-tac-toe.")
 
 moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-for move in moves:
-    input(">: ")
-    print(f"Player X or 0 said: {move}")
+for playermove in moves:
+    playermove = input(">: ")
+    print(f"Player X or 0 said: {playermove}")
 
 print("Done! Sry, I have no idea yet if anyone won.")
