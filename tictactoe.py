0a5d996ad97e5706242f0017aeba23bc3b7ed94f
Elisabeth Irgens <hello@elisabethirgens.com>
2018-02-01 18:21:22 +0100

    Rename players reserving numbers for other names 💁‍

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,7 @@
 print("Welcome! Let’s play a game of tic-tac-toe.")
 
-player1 = input("X: ")
-print(f"Player X: {player1}")
+playerA = input("X: ")
+print(f"Player X: {playerA}")
 
-player2 = input("0: ")
-print(f"Player 0: {player2}")
+playerB = input("0: ")
+print(f"Player 0: {playerB}")
