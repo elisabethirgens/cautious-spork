681c9f8f31b7960f999bca978a4757e658332bc4
Elisabeth Irgens <hello@elisabethirgens.com>
2018-02-01 19:01:02 +0100

    Change list of moves from strings to integers 👌

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,6 @@
 print("Welcome! Let’s play a game of tic-tac-toe.")
 
-moves = ['A1', 'B2', 'A3', 'B4', 'A5', 'B6', 'A7', 'B8', 'A9']
+moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 moves[0] = input("X: ")
 print(f"Player X: {moves[0]}")
