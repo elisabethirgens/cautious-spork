e678c9192e5d976a6f33c543e124b754ef0fefcc
Elisabeth Irgens <hello@elisabethirgens.com>
2018-02-01 18:29:15 +0100

    Prompt for nine moves and print them back 🙃

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,30 @@
 print("Welcome! Let’s play a game of tic-tac-toe.")
 
-playerA = input("X: ")
-print(f"Player X: {playerA}")
+playerA1 = input("X: ")
+print(f"Player X: {playerA1}")
 
-playerB = input("0: ")
-print(f"Player 0: {playerB}")
+playerB2 = input("0: ")
+print(f"Player 0: {playerB2}")
+
+playerA3 = input("X: ")
+print(f"Player X: {playerA3}")
+
+playerB4 = input("0: ")
+print(f"Player 0: {playerB4}")
+
+playerA5 = input("X: ")
+print(f"Player X: {playerA5}")
+
+playerB6 = input("0: ")
+print(f"Player 0: {playerB6}")
+
+playerA7 = input("X: ")
+print(f"Player X: {playerA7}")
+
+playerB8 = input("0: ")
+print(f"Player 0: {playerB8}")
+
+playerA9 = input("0: ")
+print(f"Player X: {playerA9}")
+
+print("Done! Sry, I have no idea yet if anyone won.")
