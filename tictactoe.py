1c2d2b565931b0e4c9c20d2f10817713a445f14e
Elisabeth Irgens <hello@elisabethirgens.com>
2018-02-01 19:13:40 +0100

    Use a for-loop cause I know what those are now 🤓

--- tictactoe.py
+++ tictactoe.py
@@ -2,31 +2,8 @@ print("Welcome! Let’s play a game of tic-tac-toe.")
 
 moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-moves[0] = input("X: ")
-print(f"Player X: {moves[0]}")
-
-moves[1] = input("0: ")
-print(f"Player 0: {moves[1]}")
-
-moves[2] = input("X: ")
-print(f"Player X: {moves[2]}")
-
-moves[3] = input("0: ")
-print(f"Player 0: {moves[3]}")
-
-moves[4] = input("X: ")
-print(f"Player X: {moves[4]}")
-
-moves[5] = input("0: ")
-print(f"Player 0: {moves[5]}")
-
-moves[6] = input("X: ")
-print(f"Player X: {moves[6]}")
-
-moves[7] = input("0: ")
-print(f"Player 0: {moves[7]}")
-
-moves[8] = input("X: ")
-print(f"Player X: {moves[8]}")
+for move in moves:
+    input(">: ")
+    print(f"Player X or 0 said: {move}")
 
 print("Done! Sry, I have no idea yet if anyone won.")
