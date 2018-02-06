5b0daac8c09b21f604628576dffd0067be5452da
Elisabeth Irgens <hello@elisabethirgens.com>
2018-02-06 20:26:49 +0100

    Initialize empty list to store player input 💃

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,10 @@
 print("Welcome! Let’s play a game of tic-tac-toe.")
 
-moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+moves = [ ]
 
-for playermove in moves:
+for playermove in range(0,9):
     playermove = input(">: ")
     print(f"Player X or 0 said: {playermove}")
+    moves.append(playermove)
 
 print("Done! Sry, I have no idea yet if anyone won.")
