5e2c7ffc9ef516d3a3d6f8631651f04b4819d5a4
Elisabeth Irgens <hello@elisabethirgens.com>
2018-02-18 19:27:53 +0100

    Start over and try out some if statements ↗️

--- tictactoe.py
+++ tictactoe.py
@@ -1,15 +1,26 @@
 def game():
+    print("Lets play! Only integers between 1-9 plz:")
 
-    moves = [ ]
+    for playermove in range(0,9):
+        playermove = int(input(">>> "))
 
-    while len(moves) < 8:
-        playermove = input("X >>> ")
-        print(f"Player X: {playermove}")
-        moves.append(playermove)
-        playermove = input("0 >>> ")
-        print(f"Player 0: {playermove}")
-        moves.append(playermove)
-
-    print(f"{moves}")
+        if playermove == 1:
+            print("Top left corner, all right. ↖️")
+        if playermove == 2:
+            print("Top center. ⬆️")
+        if playermove == 3:
+            print("Top right corner, nice. ↗️")
+        if playermove == 4:
+            print("Left center. ⬅️")
+        if playermove == 5:
+            print("Yeah that’s a good one. 🆒")
+        if playermove == 6:
+            print("Right center. ➡️")
+        if playermove == 7:
+            print("Bottom left corner, good job. ↙️")
+        if playermove == 8:
+            print("Bottom center. ⬇️")
+        if playermove == 9:
+            print("Bottom right corner, cool. ↘️")
 
 game()
