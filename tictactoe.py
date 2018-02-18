5881249ca7ba0cdbe840ef20c8ed849dfe9b5422
Elisabeth Irgens <hello@elisabethirgens.com>
2018-02-18 16:02:35 +0100

    Remove prints I don’t need anymore 🌧

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,8 @@
-print("Welcome! Let’s play a game of tic-tac-toe.")
-
 moves = { }
 
 for i in range(0,9):
     moves[i] = input(">>> ")
     print(f"Player X or 0 said: {moves[i]}")
 
-print("Done! Sry, I have no idea yet if anyone won.")
 print('=' * 40)
-print("But look! Here are your moves:")
 print(f"{moves}")
-print('=' * 40)
