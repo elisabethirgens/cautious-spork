7c444442fe45850e578afb142bf23cfa94354789
Elisabeth Irgens <hello@elisabethirgens.com>
2018-02-18 16:09:02 +0100

    Define a function even if it doesn’t do much yet 🙈

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,11 @@
-moves = { }
+def game():
+    moves = { }
 
-for i in range(0,9):
-    moves[i] = input(">>> ")
-    print(f"Player X or 0 said: {moves[i]}")
+    for i in range(0,9):
+        moves[i] = input(">>> ")
+        print(f"Player X or 0 said: {moves[i]}")
 
-print('=' * 40)
-print(f"{moves}")
+    print('=' * 40)
+    print(f"{moves}")
+
+game()
