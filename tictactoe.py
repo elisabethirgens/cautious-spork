ed4aa31ce32113274a88ca34e94a5c540f50fa1f
Elisabeth Irgens <hello@elisabethirgens.com>
2018-04-02 17:28:14 +0200

    Start coding up the pseudocode from post 👻

--- tictactoe.py
+++ tictactoe.py
@@ -1,12 +1,8 @@
-moves = []
+empty_squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-def game():
-    print("Lets play! Only integers between 1-9 plz:")
-
-    for playermove in range(0,9):
-        playermove = int(input(">>> "))
-        moves.append(playermove)
-
-game()
-
-print(moves)
+if len(empty_squares) > 1:
+    print("We’ve got empty squares. Let’s play!")
+elif len(empty_squares) == 1:
+    print("One last square. This move is given.")
+else:
+    print("No empty squares. Game over.")
