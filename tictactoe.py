8a20cd29187fe99ec07a70c4ac851ecd749bbd7b
Elisabeth Irgens <hello@elisabethirgens.com>
2018-04-02 18:51:39 +0200

    Promt for input and store move in list 📥

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,18 @@
 empty_squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+playerX_moves = []
+player0_moves = []
 next_turn_is_x = True
 
 if next_turn_is_x == True and len(empty_squares) > 1:
     print("We’ve got empty squares. Player X: ")
+    playermove = input()
+    playerX_moves.append(playermove)
+    print(">>>>>>>>>>", playerX_moves)
 elif next_turn_is_x == False and len(empty_squares) > 1:
     print("We’ve got empty squares. Player 0: ")
+    playermove = input()
+    player0_moves.append(playermove)
+    print(">>>>>>>>>>", player0_moves)
 elif len(empty_squares) == 1:
     print("One last square. This move is given.")
 else:
