5c6bbb9ff822eef9da7b8050bab46a455bf6e053
Elisabeth Irgens <hello@elisabethirgens.com>
2018-04-02 18:36:00 +0200

    Add check for who’s turn it is next 🔄

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,10 @@
 empty_squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+next_turn_is_x = True
 
-if len(empty_squares) > 1:
-    print("We’ve got empty squares. Let’s play!")
+if next_turn_is_x == True and len(empty_squares) > 1:
+    print("We’ve got empty squares. Player X: ")
+elif next_turn_is_x == False and len(empty_squares) > 1:
+    print("We’ve got empty squares. Player 0: ")
 elif len(empty_squares) == 1:
     print("One last square. This move is given.")
 else:
