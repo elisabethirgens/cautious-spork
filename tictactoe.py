eb4be3226edd9f256a623ab223397bfc663f0476
Elisabeth Irgens <hello@elisabethirgens.com>
2018-04-02 13:07:21 +0200

    Create list that can store player’s moves

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,12 @@
+moves = []
+
 def game():
     print("Lets play! Only integers between 1-9 plz:")
 
     for playermove in range(0,9):
         playermove = int(input(">>> "))
+        moves.append(playermove)
 
 game()
+
+print(moves)
